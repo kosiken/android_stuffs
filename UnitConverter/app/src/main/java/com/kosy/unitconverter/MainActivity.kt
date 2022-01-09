a1e5a41d7ced011af866bd16e7df43d06636a61f
kosiken <allisonkosy@gmail.com>
2022-01-09 12:39:32 +0100

    Refactor

--- UnitConverter/app/src/main/java/com/kosy/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/kosy/unitconverter/MainActivity.kt
@@ -1,42 +1,39 @@
 package com.kosy.unitconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import com.kosy.unitconverter.databinding.ActivityMainBinding
-import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     var ans: Float = 0.0F
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-    val binding = ActivityMainBinding.inflate(layoutInflater)
-    binding.editText.addTextChangedListener(object: TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-         }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            try {
-                val float = ("" + s).toFloat();
-                ans = float * 2.5F
-                binding.textView.setText("$ans Li")
-            }catch (exception: NumberFormatException) {
-                binding.textView.setText(s);
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
-        }
 
-        override fun afterTextChanged(s: Editable?) {
-         }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                try {
+                    val float = ("" + s).toFloat()
+                    ans = float * 2.5F
+                    binding.textView.text = "$ans Li"
+                } catch (exception: NumberFormatException) {
+                    binding.textView.text = s
+                }
+            }
 
-    })
+            override fun afterTextChanged(s: Editable?) {
+            }
 
-    setContentView(binding.root)
+        })
+
+        setContentView(binding.root)
 
 
     }
